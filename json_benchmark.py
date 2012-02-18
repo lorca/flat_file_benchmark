dcdfa74e0328d016117032d075ebe203b0deebaf
Steve A <iradik@gmail.com>
2012-02-18 06:54:22 -0800

    changing output to json, moving to different rendering for graphs..

---
+++ json_benchmark.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import pickle
+import os
+import json
+
+def plot(label, filename):
+    stat = pickle.load(open(filename))["mem"]["Active"]
+    stat = [int((int(number) - int(stat[0]))/1024.0) for number in stat]
+    return {"mem":stat[-1],"program":label,"cpu":len(stat)}
+
+filenames = []
+for root, dirs, files in os.walk("output"):
+    for name in files: filenames.append((root,name))
+
+filenames.sort()
+
+stats=[]
+for filename in filenames:
+    path = filename[0]
+    name = filename[1]
+    label = name.replace(".benchmark","")
+    stat = plot(label, path + "/" + name)
+    stats.append(stat)
+
+print json.dumps(stats)
